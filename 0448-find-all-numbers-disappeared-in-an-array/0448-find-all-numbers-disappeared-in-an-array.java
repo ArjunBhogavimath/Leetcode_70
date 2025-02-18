f0eed7a3927284230383de0ad084bcc967ca27cf
Mallikarjunaiah B M <54211632+ArjunBhogavimath@users.noreply.github.com>
2025-02-19 01:43:34 +0530

    Time: 6 ms (83.76%), Space: 56.6 MB (61.25%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,22 +1,67 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for(int num : nums){
-            set.add(num);
-        }
+        List<Integer> result_list = new ArrayList<>();
 
-        List<Integer> list = new ArrayList<>();
+        for(int i =0;i<nums.length;i++){
+            int index = Math.abs(nums[i]) - 1;
 
-        for(int i =1 ;i <= nums.length;i++){
-            if(!set.contains(i)){
-                list.add(i);
+            if(nums[index] > 0){
+                nums[index] = -nums[index];
             }
         }
-        return list;
+
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]>0){
+                result_list.add(i+1);
+            }
+        }
+        return result_list;
+
+
+        // Set<Integer> set = new HashSet<>();
+        // for(int num : nums){
+        //     set.add(num);
+        // }
+
+        // List<Integer> list = new ArrayList<>();
+
+        // for(int i =1 ;i <= nums.length;i++){
+        //     if(!set.contains(i)){
+        //         list.add(i);
+        //     }
+        // }
+        // return list;
     }
 }
 
 /**
+
 1. Brute force :
+take 2 loops 
+check each index from 1 to n and
+check if that element exists
+otherwise add it to list
+
+TC : O(N2) SC : O(1)
+
+
+2. BETTER :
+added all elemets to set -> o(n) space with o(n) time
+now o(n) taversal time to check contains
+contains will be done in o(1)
+
+so TC : o(2N) SC : O(N)
+
+
+3.OPTIMAL :
+Using input array
+loop through array 
+take index as nums[i]-1 (-1 because of index starts with 0) and abs because of negative val
+now check if (nums[index] > 0) if yes mark it as negative
+
+and later run a loop again and any items whose value is in posive add it to list
+and return list
+
+TC : O(N) SC: O(1)
 
  */
