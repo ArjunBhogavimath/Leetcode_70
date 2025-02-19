a0b1f903cab28984dab13f33e7068c530bf471c9
Mallikarjunaiah B M <54211632+ArjunBhogavimath@users.noreply.github.com>
2025-02-19 16:44:59 +0530

    Time: 10 ms (20.9%), Space: 44.4 MB (74.41%) - LeetHub

---
+++ 1482-how-many-numbers-are-smaller-than-the-current-number/1482-how-many-numbers-are-smaller-than-the-current-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n = nums.length;
+        int ans_arr[] = new int[n];
+        for(int i =0;i<n;i++){
+            int count = 0;
+            for(int j =0;j<n;j++){
+                if(i != j && nums[j] < nums[i]){
+                    count++;
+                }
+            }
+            ans_arr[i] = count;
+        }
+        return ans_arr;
+    }
+}
+
+/**
+
+1.Brute force : using 2 loops
+create new ans arr with same lenght
+now run 2 loops 
+and inside inner loop
+add a condition that ignores when i == j and nums[i] > nums[j]
+and increments counter when passed.
+now we have number of smaller elements count
+we will add that counter to ith position of ans array
+
+TC : O(N^2) SC: O(1) as the ans array is response
+ */
