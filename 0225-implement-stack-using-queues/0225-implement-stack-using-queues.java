4b57edef6c23028b8566aae80263e4ea541d63eb
Mallikarjunaiah B M <54211632+ArjunBhogavimath@users.noreply.github.com>
2025-03-15 00:16:15 +0530

    Time: 0 ms (100%), Space: 41.2 MB (80.42%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -36,3 +36,14 @@ class MyStack {
  * int param_3 = obj.top();
  * boolean param_4 = obj.empty();
  */
+
+ /**
+ easiest way
+ only applies while pushing
+ step 1 : add an element to queue
+ step 2: get the length of the que and substrcat it by 1
+ 3 : if the length is above 0 then there will be multiple elements in queue
+ 4 : so now run a while loop till length becomes 0 
+ 5 : inside that pop the first queue element (1st one removes) and add it back(so it will reach top)
+ 6 : thats how we will make proper stack using queue
+  */
