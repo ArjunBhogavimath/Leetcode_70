711304a8f8730031a6d808bcd186c88dde621d04
Mallikarjunaiah B M <54211632+ArjunBhogavimath@users.noreply.github.com>
2025-03-15 23:25:44 +0530

    Time: 0 ms (100%), Space: 41.5 MB (47.11%) - LeetHub

--- 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
@@ -1,24 +1,37 @@
 class Solution {
     public int timeRequiredToBuy(int[] tickets, int k) {
-        Queue<Integer> que = new LinkedList<>();
-        int n = tickets.length;
-        for(int i=0;i<n;i++){
-            que.offer(i);
-        }
         int time = 0;
 
-        while(!que.isEmpty()){
-            int person = que.poll();
-            if(tickets[person] > 0){
-                tickets[person]--;
-                time++;
-                if(tickets[person] > 0){
-                    que.offer(person);
-                }
+        for(int i=0;i<tickets.length;i++){
+            if(i<=k){
+                time += Math.min(tickets[k], tickets[i]);
+            } else{
+                time += Math.min(tickets[k]-1, tickets[i]);
             }
-            if(tickets[k] == 0) return time;
         }
         return time;
+
+
+        //Brute force using Queue
+        // Queue<Integer> que = new LinkedList<>();
+        // int n = tickets.length;
+        // for(int i=0;i<n;i++){
+        //     que.offer(i);
+        // }
+        // int time = 0;
+
+        // while(!que.isEmpty()){
+        //     int person = que.poll();
+        //     if(tickets[person] > 0){
+        //         tickets[person]--;
+        //         time++;
+        //         if(tickets[person] > 0){
+        //             que.offer(person);
+        //         }
+        //     }
+        //     if(tickets[k] == 0) return time;
+        // }
+        // return time;
     }
 }
 
