e89c0d4004e966b0225eb8bf59cc93fa4274f5c1
Mallikarjunaiah B M <54211632+ArjunBhogavimath@users.noreply.github.com>
2025-03-15 23:00:33 +0530

    Time: 11 ms (13.86%), Space: 44.9 MB (5.39%) - LeetHub

---
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        Queue<Integer> que = new LinkedList<>();
+        int n = tickets.length;
+        for(int i=0;i<n;i++){
+            que.offer(i);
+        }
+        int time = 0;
+
+        while(!que.isEmpty()){
+            int person = que.poll();
+            if(tickets[person] > 0){
+                tickets[person]--;
+                time++;
+                if(tickets[person] > 0){
+                    que.offer(person);
+                }
+            }
+            if(tickets[k] == 0) return time;
+        }
+        return time;
+    }
+}
+
+
+/**
+1.Naive solution : using queue
+i need to add elements inidces to the queue (not elements)
+queue -> 1,2,3,4
+
+now run a while loop (will stop when all the tickets completed)
+and pop the elements
+and get that indices element in array
+now decrease it by 1 and add time if the element > 0
+and now check agaun after decreasing if it became 0
+otherwise add it to the queue
+now check again if tickets[k] == 0
+if yes return time
+
+ */
