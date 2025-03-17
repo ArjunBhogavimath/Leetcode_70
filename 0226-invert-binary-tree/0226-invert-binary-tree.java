b996b2a5c442fca2d770ee2a73c7071d5f2e51ba
Mallikarjunaiah B M <54211632+ArjunBhogavimath@users.noreply.github.com>
2025-03-17 17:43:15 +0530

    Time: 0 ms (100%), Space: 41.4 MB (34.56%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -15,17 +15,28 @@
  */
 class Solution {
     public TreeNode invertTree(TreeNode root) {
-        invert(root);
+       if(root == null) return null;
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        
+        root.left = right;
+        root.right = left;
         return root;
     }
-    public void invert(TreeNode root){
-        if(root == null) return;
+    // public void invert(TreeNode root){
+    //     if(root == null) return;
 
-        TreeNode temp = root.left;
-        root.left = root.right;
-        root.right = temp;
+    //     TreeNode temp = root.left;
+    //     root.left = root.right;
+    //     root.right = temp;
 
-        invert(root.left);
-        invert(root.right);
-    }
+    //     invert(root.left);
+    //     invert(root.right);
+    // }
 }
+/**
+
+to invert a binary tree
+i can just swap left with right node recursively
+ */
