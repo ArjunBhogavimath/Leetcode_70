aa45703770957be0a3cffdc284345a304ca4400c
Mallikarjunaiah B M <54211632+ArjunBhogavimath@users.noreply.github.com>
2025-03-20 00:28:24 +0530

    Time: 0 ms (100%), Space: 45.4 MB (54.61%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.java
@@ -17,10 +17,21 @@ class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
         if(root == null || root.val == val) return root;
 
-        if(root.val < val){
-            return searchBST(root.right, val);
-        } else{
-            return searchBST(root.left, val);
-        }
+        return root.val < val ? searchBST(root.right, val) : searchBST(root.left, val);
+        
+
+        // if(root.val < val){
+        //     return searchBST(root.right, val);
+        // } else{
+        //     return searchBST(root.left, val);
+        // }
     }
 }
+
+/**
+
+the main advantage we get here
+earlier in binary tree we need to check both sides
+but here based on the value we can restict to that side
+so we can get the data  in O(logN) time
+ */
