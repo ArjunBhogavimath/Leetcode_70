460b998c3e66d303864e9e9d6787338868e83523
Mallikarjunaiah B M <54211632+ArjunBhogavimath@users.noreply.github.com>
2025-08-23 23:57:40 +0530

    Time: 1 ms (99.67%), Space: 45.9 MB (85.98%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,7 +1,8 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         int writePos = 0;
-
+        //on 1st traverse write non zero elements to array
+        //after that in 2nd traversal add zero's to the remaining places
         for(int readPos = 0 ; readPos < nums.length ; readPos++){
             if(nums[readPos] != 0){
                 nums[writePos++] = nums[readPos];
