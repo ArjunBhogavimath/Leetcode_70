fbfcf4240541c638ad272ed9a550282016ffec75
Mallikarjunaiah B M <54211632+ArjunBhogavimath@users.noreply.github.com>
2025-08-29 21:48:46 +0530

    Time: 61 ms (52.56%), Space: 61.7 MB (22.52%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,25 +1,13 @@
 class Solution {
-    public int findKthLargest(int[] nums, int k) {  
+    public int findKthLargest(int[] nums, int k) {
         Queue<Integer> pq = new PriorityQueue<>();
-        for(int i=0;i<nums.length;i++){
+
+        for(int i =0;i<nums.length;i++){
             pq.add(nums[i]);
-            if(pq.size() > k){
-                pq.poll();
+            if(pq.size() > k) {
+                pq.remove();
             }
         }
-        return pq.peek(); 
+        return pq.remove();
     }
 }
-
-/**
-1. Brute force:
-sort the array and return k-1th element.
-TC:O(NlogN) SC: O(1)
-
-2. Using max_heap:
-use priority queue, add elements to the queue
-if the size is greater than k
-start ppping element(which will pop largest element)
-so at last we will have our kth smallest at top of the queue.
-TC: O(N) SC:O(K)
- */
