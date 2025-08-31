69ceba13bdb31805396ba5b65bf8c152a81d8f95
Mallikarjunaiah B M <54211632+ArjunBhogavimath@users.noreply.github.com>
2025-08-31 22:57:09 +0530

    Time: 29 ms (16.22%), Space: 51 MB (56.15%) - LeetHub

---
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int dp[][] = new int[text1.length()][text2.length()];
+        for(int row[] : dp){
+            Arrays.fill(row, -1);
+        }
+        return lcsRecursion(text1, text2, 0, 0, dp);
+    }
+    private int lcsRecursion(String text1, String text2, int i, int j, int dp[][]){
+        if(i >= text1.length()  || j >= text2.length()) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(text1.charAt(i) == text2.charAt(j)){
+            dp[i][j] = 1 + lcsRecursion(text1, text2, i+1,j+1, dp);
+        } 
+
+        else{
+            dp[i][j] = Math.max(lcsRecursion(text1, text2, i+1,j,dp), lcsRecursion(text1, text2, i,j+1, dp));
+        }
+
+        return dp[i][j];
+    }
+}
