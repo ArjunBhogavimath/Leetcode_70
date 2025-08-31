f23945736d365f8e4c7988de05058f537c0ef5a5
Mallikarjunaiah B M <54211632+ArjunBhogavimath@users.noreply.github.com>
2025-09-01 01:18:51 +0530

    Time: 19 ms (87.07%), Space: 50.9 MB (67.82%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,5 +1,49 @@
 class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+
+        int dp[][] = new int[m+1][n+1];
+
+
+        //in the dp we should fill the 1st row and column with 0
+        //bug we can ignore this
+        // for(int i = 0;i<m;i++){
+        //     dp[i][0] = 0;
+        // }
+        // for(int i = 0;i<n;i++){
+        //     dp[0][i] = 0;
+        // }
+
+        /**
+        now run a for loops and start comparing,
+        and if matches add 1 with previous diagonal element
+        if not match then choose max of the either previous row or previous column
+        at last return the last index of the dp matrix
+         */
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } 
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        //return last value (which will be maximum)
+        return dp[m][n];
+    }
+
+
+
+
+
+
+
+    //This is for top down approach of DP
+
+    public int longestCommonSubsequenceForTopDown(String text1, String text2) {
         //create a memoization matrix to store the values
         int dp[][] = new int[text1.length()][text2.length()];
         //fill them with -1
