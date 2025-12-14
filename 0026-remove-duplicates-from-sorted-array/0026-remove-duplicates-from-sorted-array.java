d26f5ce16a0b08535b9a8e5fb67b0a60cefa1f24
Mallikarjunaiah B M <54211632+ArjunBhogavimath@users.noreply.github.com>
2025-12-14 21:12:42 +0530

    Time: 3 ms (7.56%), Space: 46.8 MB (23.43%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,23 +1,19 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         int n = nums.length;
-        int left = 0; //pointing towards unique elements 
-        for(int right = 1;right<n;right++){
-            if(nums[left] != nums[right]){
+        int left = 0;
+        int right = 1;
+        
+        while(left<right && right<n){
+            if(nums[left] == nums[right]){
+                right++;
+            }
+            else {
                 nums[++left] = nums[right];
+                right++;
             }
         }
+        System.out.println(left);
         return left+1;
     }
 }
-
-/**
-2 pointers:
-1 will be always pointing towards unique elements from left
-another pointer will be checking next greater element
-whenever there is a next greater element I'll copy it to 1st pointer's next place
-
-once the second pointer reaches end,
-i'll exit the loop
-and now from the next element from 1st pointer make them as -1
- */
