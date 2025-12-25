c6df177adbcc79d64e39ed8d66c7cfa4ad76c26e
Mallikarjunaiah B M <54211632+ArjunBhogavimath@users.noreply.github.com>
2025-12-26 01:32:46 +0530

    Time: 1 ms (99.89%), Space: 47.3 MB (5.33%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,15 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int xor = 0;
-        for(int i = 0;i<nums.length;i++){
-            xor^=nums[i];
+        int n = nums.length;
+        int single_num = 0;
+        for(int i =0;i<n;i++){
+            single_num ^= nums[i];
         }
-        return xor;
+        return single_num;
     }
 }
-
-/**
-do the xor operation whenever we want 1 unique element
-if the duplicte elements came, they cancel each other.
-so at end the unique element will be remaining in xor variable
- */
